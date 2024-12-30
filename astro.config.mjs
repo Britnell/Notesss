14bb826c3ec9a9acb5b7418fed4e4a63471339f6
Thomas Britnell <thomas.britnell@h-d-gmbh.de>
2024-12-30 14:15:31 +0100

    add vercel intefration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: "standalone" }),
-  integrations: [tailwind(), preact({ compat: true }), 
-  ],
-  output: 'server'
+  adapter: vercel({}),
+  integrations: [tailwind(), preact({ compat: true })],
+  output: "server",
 });

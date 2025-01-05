fb35738d0ed4fc043f55d126e8c4bcbdef0954d4
Thomas Britnell <thomas.britnell@h-d-gmbh.de>
2025-01-05 15:05:22 +0100

    tweak regex, was using 0+ isntead of 1+

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -1,6 +1,6 @@
-export const regHeading = /^(\#{1,3})\s(.*)$/;
+export const regHeading = /^(\#{1,3})\s(.+)$/;
 export const regList = /^\s?[-*]\s(.*)$/;
-export const regTodo = /^-\s*\[([xX ]?)\]\s*(.*)$/;
+export const regTodo = /^-\[([x ]?)\]\s+(.+)$/;
 
 export const regHabit = /\B\[([A-Z][a-z]*)(\d*)\]\B/;
 export const regHabits = /\B\[([A-Z][a-z]*)(\d*)\]\B/g;

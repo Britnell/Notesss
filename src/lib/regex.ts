c4dd4e15bb7aec9105a04b4938757bf52a4a52c1
Thomas Britnell <thomas.britnell@h-d-gmbh.de>
2025-03-01 10:15:24 +0100

    fix habit regex

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -2,8 +2,8 @@ export const regHeading = /^(\#{1,3})\s(.+)$/;
 export const regList = /^\s?[-*]\s(.*)$/;
 export const regTodo = /^-\[([x ]?)\]\s+(.+)$/;
 
-export const regHabit = /\B[^-]\[([A-Za-z][a-z]*)(\d*)\]\B/;
-export const regHabits = /\B[^-]\[([A-Za-z][a-z]*)(\d*)\]\B/g;
+export const regHabit = /(?:^|\s)\[([A-Za-z][a-z]*)(\d*)\]\B/;
+export const regHabits = /(?:^|\s)\[([A-Za-z][a-z]*)(\d*)\]\B/g;
 
 export const regTag = /\B\#(\w+)/;
 export const regTags = /\B\#\w+/g;
